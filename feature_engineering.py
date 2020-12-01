07e4d52b181d18c783dc58622e48d6250b6b7746
Tong Wu <tongwu.github@outlook.com>
2020-12-02 07:26:35 +0800

    add merge

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import gc
 from utils import reduce_mem_usage
 
 def read_data():
@@ -15,17 +16,43 @@ def read_data():
 
     return calendar, sell_prices, train_data
 
-def melt_train_data(train_data):
-    train_data = pd.melt(train_data, id_vars = ['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id'], var_name = 'day', value_name = 'demand')
-    train_data = reduce_mem_usage(train_data)
+def melt_train_data(input_data):
+    data = pd.melt(input_data, id_vars = ['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id'], var_name = 'day', value_name = 'demand')
+    data = reduce_mem_usage(data)
+
+    print('melt_train_data has {} rows and {} columns'.format(data.shape[0], data.shape[1]))
+    del input_data
+    gc.collect()
+
+    return data
+
+def merge_with_calendar(input_data, calendar):
+    data = pd.merge(input_data, calendar, how = 'left', left_on = ['day'], right_on = ['d'])
+    data.drop(['d', 'day'], inplace = True, axis = 1)
+    print('Our final dataset to train has {} rows and {} columns'.format(data.shape[0], data.shape[1]))
+
+    del input_data
+    gc.collect()
+
+    return data
+
+def merge_with_sales(input_data, sell_prices):
+    data = input_data.merge(sell_prices, on = ['store_id', 'item_id', 'wm_yr_wk'], how = 'left')
+    print('Our final dataset to train has {} rows and {} columns'.format(data.shape[0], data.shape[1]))
     
-    return train_data
+    del input_data
+    gc.collect()
+
+    return data
 
 def load_train_data():
     calendar, sell_prices, train_data = read_data()
-
     train_data = melt_train_data(train_data)
+    train_data = merge_with_calendar(train_data, calendar)
+    train_data = merge_with_sales(train_data, sell_prices)
+
     print(train_data.head())
+
 
 
 
